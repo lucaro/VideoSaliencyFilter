9c72bb0dc24119ff5f6d5adc1c5dc1c08365d4cb
Luca Rossetto <rossetto@ifi.uzh.ch>
2019-10-31 13:18:35 +0100

    First iteration of modifying audio and video

--- src/main/java/ch/lucaro/videosaliency/Main.java
+++ src/main/java/ch/lucaro/videosaliency/Main.java
@@ -1,19 +1,15 @@
 package ch.lucaro.videosaliency;
 
 
-import ch.lucaro.videosaliency.util.FloatArrayImageUtil;
 import org.vitrivr.cineast.core.config.CacheConfig;
 import org.vitrivr.cineast.core.config.DecoderConfig;
+import org.vitrivr.cineast.core.data.frames.AudioFrame;
 import org.vitrivr.cineast.core.data.frames.VideoFrame;
 import org.vitrivr.cineast.core.data.raw.CachedDataFactory;
-import org.vitrivr.cineast.core.data.raw.images.MultiImage;
 import org.vitrivr.cineast.core.extraction.decode.video.FFMpegVideoDecoder;
 import org.vitrivr.cineast.core.extraction.decode.video.FFMpegVideoEncoder;
 
-import javax.imageio.ImageIO;
-
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.LinkedList;
@@ -23,11 +19,10 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private static ImagePipeline pipeline = new ImagePipeline(5, 3f, .1f);
+    private static ImagePipeline imagePipeline = new ImagePipeline(5, 3f, .1f);
+    private static AudioPipeline audioPipeline = new AudioPipeline();
     private static SaliencyMask saliencyMask;
-    private static File outputDir = new File("tmp");
 
-    private static int frameCounter = 0;
 
     public static void main(String[] args) throws IOException {
 
@@ -48,7 +43,7 @@ public class Main {
         }
         frames.add(frame);
 
-        FFMpegVideoEncoder encoder = new FFMpegVideoEncoder(frame.getImage().getWidth(), frame.getImage().getHeight(), (int)frame.getDescriptor().getFps(), 44100, "out.mp4", false);
+        FFMpegVideoEncoder encoder = new FFMpegVideoEncoder(frame.getImage().getWidth(), frame.getImage().getHeight(), (int)frame.getDescriptor().getFps(), 44100, "out.mp4", true);
 
         while ((frame = decoder.getNext()) != null){
             frames.add(frame);
@@ -73,10 +68,18 @@ public class Main {
         float[][][] masks = saliencyMask.process(frames.stream().map(VideoFrame::getImage).collect(Collectors.toList()));
 
         for (int i = 0; i < masks.length; ++i){
-            BufferedImage out = pipeline.process(frames.get(i).getImage().getBufferedImage(), masks[i]);
+            BufferedImage out = imagePipeline.process(frames.get(i).getImage().getBufferedImage(), masks[i]);
 
             encoder.add(CachedDataFactory.DEFAULT_INSTANCE.newInMemoryMultiImage(out));
 
+        }
+
+        for (VideoFrame frame: frames){
+            if(frame.getAudio().isPresent()){
+                for(AudioFrame audioFrame : audioPipeline.process(frame.getAudio().get())) {
+                    encoder.add(audioFrame);
+                }
+            }
         }
 
         System.out.println("---> Processed batch in " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds");
