675a77e7525290ce675bce42377c8bd827ee9cd7
Luca Rossetto <rossetto@ifi.uzh.ch>
2019-12-04 11:22:15 +0100

    Added simple time estimation

--- src/main/java/ch/lucaro/videosaliency/Main.java
+++ src/main/java/ch/lucaro/videosaliency/Main.java
@@ -64,6 +64,8 @@ public class Main {
 
         VideoFrame frame = decoder.getNext();
 
+        long startTime = System.currentTimeMillis();
+
         if (frame == null){
             return;
         }
@@ -76,7 +78,7 @@ public class Main {
             ++currentFrameCount;
             if (frames.size() >= 21){
                 processBatch(frames, encoder);
-                LOGGER.info("Processed {} of {} frames", currentFrameCount, totalFrameCount);
+                LOGGER.info(getTimeEstimate(totalFrameCount, currentFrameCount, startTime));
                 while (frames.size() > 5){
                     frames.poll();
                 }
@@ -112,6 +114,17 @@ public class Main {
 
         LOGGER.info("Processed batch in {} seconds", (System.currentTimeMillis() - startTime) / 1000);
 
+    }
+
+    private static String getTimeEstimate(int totalFrameCount, int currentFrame, long startMillis){
+        totalFrameCount = Math.max(totalFrameCount, 1);
+
+        float progress = ((float) currentFrame) / totalFrameCount;
+        int elapsed = (int) (System.currentTimeMillis() - startMillis) / 1000;
+
+        int remaining = (int) (elapsed / progress);
+
+        return String.format("Processed " + currentFrame + " of " + totalFrameCount + " frames, (%,.2f%%) remaining time: %02d:%02d:%02d", progress, remaining / 3600, (remaining % 3600) / 60, remaining % 60);
     }
 
 }
